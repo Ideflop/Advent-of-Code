ef968e79d44547ec704a0e4e9348e96b127559fd
Ideflop <vi03pl@gmail.com>
2023-12-27 21:01:14 +0100

    add templates files

---
+++ templates/day-x/src/part2.rs
@@ -0,0 +1,15 @@
+pub fn process(input: &str) -> String {
+    let mut result = 0;
+    return result.to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "";
+        assert_eq!("", process(input));
+    }
+}
